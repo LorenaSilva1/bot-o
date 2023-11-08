e8ecec592146b7ca669ef55b0d252647e0535443
LorenaSilva1 <lorenadasilvasantosloh@gmail.com>
2023-11-08 08:43:25 -0300

    teste2

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ function addToInput(letter) {
 function saveWord() {
     const input = document.getElementById('input');
     const word = input.value;
-    // Aqui você pode fazer algo com a palavra, como enviá-la para um servidor ou exibi-la na página.
+   
     alert('Palavra salva: ' + word);
 }
